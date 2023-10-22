2ba1f651a44dea8897dfef04741d9c8a26823a20
teplostanski <teplostanski@yandex.ru>
2023-10-22 04:06:34 +0300

    chore(dont up version): upgrade removeOldDocs script

--- scripts/removeOldDocs.cjs
+++ scripts/removeOldDocs.cjs
@@ -2,25 +2,84 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-const docsDirectory = 'docs/'
+/**
+ * Retrieves the current version from the package.json.
+ * @param {string} directoryPath - The path to the directory containing package.json.
+ * @returns {Array<number>} - An array containing the major, minor, and patch version numbers.
+ * @example
+ * getCurrentVersion('./') // might return [1, 2, 3] for version "1.2.3"
+ */
+async function getCurrentVersion(directoryPath) {
+  const { version } = require(path.join(directoryPath, 'package.json'))
+  return version.split('.').map(Number)
+}
 
-async function cleanOldPatchVersions() {
-  const { version } = require('../package.json')
-  const [major, minor] = version.split('.').slice(0, 2)
+/**
+ * Lists all directories in the given path.
+ * @param {string} directoryPath - The path to list directories from.
+ * @returns {Array<string>} - An array of directory names.
+ * @example
+ * getDirectories('./docs/') // might return ['0.2.1', '0.2.2', '0.2.3']
+ */
+async function getDirectories(directoryPath) {
+  return await fs.readdir(directoryPath)
+}
 
-  const directories = await fs.readdir(docsDirectory)
+/**
+ * Identifies outdated directories based on current version and whether to consider minor versions.
+ * @param {Array<string>} directories - A list of directory names.
+ * @param {Array<number>} currentVersion - The current version as [major, minor, patch].
+ * @param {boolean} cleanMinor - Whether to consider cleaning minor versions.
+ * @returns {Array<string>} - A list of outdated directories.
+ * @example
+ * getOutdatedDirectories(['0.2.1', '0.2.2', '0.3.0'], [0, 2, 3], false) // returns ['0.2.1', '0.2.2']
+ */
+function getOutdatedDirectories(directories, currentVersion, cleanMinor) {
+  const [major, minor] = currentVersion
 
-  const outdatedDirs = directories.filter((dir) => {
-    const [dirMajor, dirMinor, dirPatch] = dir.split('.')
-    return dirMajor === major && dirMinor === minor && parseInt(dirPatch, 10) < parseInt(version.split('.')[2], 10)
+  return directories.filter((dir) => {
+    const [dirMajor, dirMinor, dirPatch] = dir.split('.').map(Number)
+
+    if (cleanMinor) {
+      return dirMajor === major && (dirMinor < minor || (dirMinor === minor && dirPatch < currentVersion[2]))
+    }
+
+    return dirMajor === major && dirMinor === minor && dirPatch < currentVersion[2]
   })
+}
 
-  for (const dir of outdatedDirs) {
-    await fs.remove(path.join(docsDirectory, dir))
+/**
+ * Removes specified directories from the given path.
+ * @param {string} directoryPath - Base path of the directories.
+ * @param {Array<string>} dirsToRemove - List of directories to be removed.
+ * @example
+ * removeDirectories('./docs/', ['0.2.1', '0.2.2']) // removes directories './docs/0.2.1/' and './docs/0.2.2/'
+ */
+async function removeDirectories(directoryPath, dirsToRemove) {
+  for (const dir of dirsToRemove) {
+    await fs.remove(path.join(directoryPath, dir))
     console.log(`Removed outdated version directory: ${dir}`)
   }
 }
 
-cleanOldPatchVersions().catch((err) => {
+/**
+ * Main function to manage the cleaning process based on command line arguments.
+ * @example
+ * node scriptName.js --clean-minor // cleans outdated patch and minor versions under default 'docs/' directory
+ * node scriptName.js ./customPath/ --clean-minor // cleans outdated patch and minor versions under specified directory
+ */
+async function main() {
+  const args = process.argv.slice(2)
+  const directoryPath = args.find((arg) => !arg.startsWith('--')) || 'docs/'
+  const cleanMinor = args.includes('--clean-minor')
+
+  const currentVersion = await getCurrentVersion(directoryPath)
+  const directories = await getDirectories(directoryPath)
+  const outdatedDirs = getOutdatedDirectories(directories, currentVersion, cleanMinor)
+
+  await removeDirectories(directoryPath, outdatedDirs)
+}
+
+main().catch((err) => {
   console.error('Failed to clean old documentation versions:', err)
 })
